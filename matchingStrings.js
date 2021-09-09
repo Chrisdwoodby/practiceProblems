0bb9186d2eec34c1c27c1818b4e1ecc66e443256
chrisdwoodby <chrisdwoodby@gmail.com>
2021-09-09 16:56:05 -0500

    matchingStrings test 10/13

---
+++ matchingStrings.js
@@ -0,0 +1,21 @@
+var matchingStrings = function(strings, queries) {
+  // Write your code here
+  // returns a new array with the # of times the queries appear in strings array
+  var result = [];
+  var tracker = {};
+
+  for (var i = 0; i < queries.length; i++) {
+    var count = 0;
+    for (var j = 0; j < strings.length; j++) {
+      if (queries[i] === strings[j] && !tracker[queries[i]]) {
+        tracker[queries[i]] = 1;
+      } else if (queries[i] === strings[j] && tracker[queries[i]]) {
+        tracker[queries[i]] ++;
+
+      } else if (strings.indexOf(queries[i]) === -1) {
+        tracker[queries[i]] = 0;
+      }
+    }
+  }
+  return Object.values(tracker);
+};
