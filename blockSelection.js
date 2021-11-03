184410834e4ca62f5a7e50b61afbf0ba79b8c1e0
chrisdwoodby <chrisdwoodby@gmail.com>
2021-11-03 15:41:16 -0500

    passing additional test cases

--- blockSelection.js
+++ blockSelection.js
@@ -4,29 +4,34 @@ const interview = function(street, req) {
 
   for (var i = 0; i < street.length; i++) {
     for (var keys in street[i]) {
-      container.push([street[i][keys], i])
+      container.push([street[i][keys], i, keys])
     }
   }
   var first = null;
   var second = null;
   var nextSet = false;
   for (var i = 0; i < container.length; i++) {
-    if (container[i][0] && first === null && !nextSet) {
-      first = container[i][1];
-    } else if (container[i][0] && second === null && !nextSet) {
-      second = container[i][1];
-      nextSet = true;
-    } else {
-      if (container[i][0] && nextSet) {
-      first = container[i][1];
-      } else if (container[i][0] && nextSet) {
-      second = container[i][1];
+    if (req.includes(container[i][2])) {
+      if (container[i][0] && first === null && !nextSet) {
+        first = container[i][1];
+      } else if (container[i][0] && second === null && !nextSet) {
+        second = container[i][1];
+        nextSet = true;
+      } else {
+        if (container[i][0] && nextSet) {
+        first = container[i][1];
+        } else if (container[i][0] && nextSet) {
+        second = container[i][1];
+        }
       }
     }
   }
-  console.log('first: ' + first, second)
+//   console.log('first: ' + first, second)
+  if (first || second === null) {
+    return (first || second);
+  }
   if (first === second) {
-    return first || second;
+    return first;
   } else {
     return Math.floor((second - first) / 2) + first;
   }
